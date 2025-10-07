754b2c20a0f87df070fc91021f6eb6d08a571aae
Omkar Shewale <shewaleomkar1@gmail.com>
2025-10-07 12:10:05 +0530

    my side i done i crated one file

---
+++ array2.java
@@ -0,0 +1,15 @@
+public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr=new int[5];
+        System.out.println("enter an elements in array");
+        for(int i=0; i < arr.length; i++){
+            arr[i]= sc.nextInt();
+            //oSystem.out.println(sc.next());
+         }
+         System.out.println("Display Array elements:");
+         for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+"");
+         }
+       // System.out.println("array elements are"+'1' +'2' +'3);
+        
+    }
